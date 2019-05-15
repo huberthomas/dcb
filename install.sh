4fd206455c7d89a1f1da4242f8db909f08cbd613
Thomas Huber <huber.thomas@outlook.com>
2019-05-15 15:12:43 +0200

    added hed dependencies

--- install.sh
+++ install.sh
@@ -4,5 +4,9 @@ cp -r ced/examples/ced/ caffe/examples/
 cp -r ced/include/ caffe/
 cp -r ced/src/ caffe/
 
+cp -r hed/examples/hed/ caffe/examples/
+cp -r hed/include/caffe/ caffe/include/
+cp -r hed/src/caffe/ caffe/src/
+
 echo 'Now download pretrained HED model hed_vgg16.caffemodel from https://drive.google.com/file/d/1R_kUd028OMWnPSRNBJTEkUQDrYOoSP7P/view?usp=sharing and put it to caffe/examples/hed'
 echo 'Finished'
